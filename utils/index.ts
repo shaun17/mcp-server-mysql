f06ee79d51d5529e43ca32e7bdf49726f929664e
Ben Borla <benborla@icloud.com>
2025-05-25 19:27:00 +0800

    allowed blank passwords

--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,21 @@
-type LogType = 'info' | 'error'
+type LogType = "info" | "error";
 
 // @INFO: Enable logging if ENABLE_LOGGING is true
-const ENABLE_LOGGING = ["true", "1"].includes(process.env.ENABLE_LOGGING || 'false')
+const ENABLE_LOGGING = ["true", "1"].includes(
+  process.env.ENABLE_LOGGING || "false",
+);
 
-export function log(type: LogType = 'info', ...args: any[]): void {
-  if (!ENABLE_LOGGING) return
+export function log(type: LogType = "info", ...args: any[]): void {
+  if (!ENABLE_LOGGING) return;
 
   switch (type) {
-    case 'info':
-      console.info(...args)
-      break
-    case 'error':
-      console.error(...args)
-      break
+    case "info":
+      console.info(...args);
+      break;
+    case "error":
+      console.error(...args);
+      break;
     default:
-      console.log(...args)
+      console.log(...args);
   }
 }
