514d4bdf2504c44c18582f7341fdef3236029016
Ben Borla <benborla@icloud.com>
2025-06-26 13:30:04 +0800

    fix linter

---
+++ TODO.md
@@ -0,0 +1,8 @@
+Replace configuration with connection string
+the configuration will look like this
+
+
+db_name:connection_string
+db_name_can_insert:boolean
+db_name_can_delete:boolean
+db_name_can_update:boolean
---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-console": "off",
+    },
+  },
+];
